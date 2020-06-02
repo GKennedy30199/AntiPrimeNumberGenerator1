6c582e4615996c09c61d8b1a81a05d1a2bf690a4
gkennedy982 <gkennedy982@gmail.com>
2020-06-02 01:13:52 +0100

    Number generator made

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace AntiPrime
 {
@@ -10,6 +11,29 @@ namespace AntiPrime
     {
         static void Main(string[] args)
         {
+            Random num = new Random();
+            int Numb = num.Next(1, 11);
+            int[] prime = new int[] { 2, 3, 5, 7 };
+            
+            string HopefullyNotAPrime = Numb.ToString();
+            Console.WriteLine("Your number is:{0}", HopefullyNotAPrime);
+            Console.ReadKey();
+            if(Numb==2)
+            {
+                //this.Close()
+            }
+            if (Numb == 3)
+            {
+                
+            }
+            if (Numb == 5)
+            {
+               
+            }
+            if (Numb == 7)
+            {
+                
+            }
         }
     }
 }
